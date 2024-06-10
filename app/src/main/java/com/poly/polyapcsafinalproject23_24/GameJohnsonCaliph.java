894db8ae9ad18ac621ebe56238fea37b80d1d3d7
Caliph Johnson <lilleafrunsfast@gmail.com>
2024-06-10 13:32:58 -0700

    done

--- app/src/main/java/com/poly/polyapcsafinalproject23_24/GameJohnsonCaliph.java
+++ app/src/main/java/com/poly/polyapcsafinalproject23_24/GameJohnsonCaliph.java
@@ -39,7 +39,8 @@ public class GameJohnsonCaliph extends GameActivity {
         btn2= findViewById(R.id.btn_2);
         btn3= findViewById(R.id.btn_3);
 
-        tvTitle.setText("Chores");
+        tvTitle.setText("Sunday Chore");
+        tvSubtitle.setText("Let do your do Chore");
         //initialize number of lives
         numLives = 3;
         //create a scanner object for user input
@@ -61,17 +62,39 @@ public class GameJohnsonCaliph extends GameActivity {
         isWon = false;
 
 
+        ivStory.setImageResource(R.drawable.im_johnsoncaliph_chore_day);
         tvStoryText.setText("It's chore day. let's go on a Chore sweep!");
 
         setAllBtnVisible();
-        btn1.setText("Go clean dog poop");
-        btn2.setText("Go clean my room");
-        btn3.setText("use your Hand ");
+        btn1.setText("let do your chore");
+        btn2.setVisibility(View.INVISIBLE);
+        btn3.setVisibility(View.INVISIBLE);
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 doYourChores();
+            }
+        });
+
+
+
+
+    }
+    private void doYourChores()
+    {
+        ivStory.setImageResource(R.drawable.im_johnsoncaliph_do_chore);
+        tvStoryText.setText("\nYou picked to do your chores and the first thing you do is clean your room \n1. Clean the bed\n2. Clean the desk \n3.cleanTheLivingRoom ");
+
+        setAllBtnVisible();
+        btn1.setText("Clean the Desk");
+        btn2.setText("Go clean my room");
+        btn3.setText("Go clean the living room ");
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cleanTheDesk();
             }
         });
 
@@ -86,26 +109,15 @@ public class GameJohnsonCaliph extends GameActivity {
         btn3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                useYourHands();
+                cleanTheLivingRoom();
             }
         });
-
-    }
-    private void doYourChores()
-    {
-
-        tvStoryText.setText("\nYou picked to do your chores and the first thing you do is clean your room");
-        tvStoryText.setText("1. Clean the bed\n2. Clean the desk");
-
-        setAllBtnVisible();
-        btn1.setText("Go clean dog poop");
-        btn2.setText("Go clean my room");
-        btn3.setText("Use your hands");
 
     }
 
     private void cleanTheBed()
     {
+        ivStory.setImageResource(R.drawable.im_johnsoncaliph_hurt);
         tvStoryText.setText("\nYou picked to clean the bed but you accidentally slipped and fell and broke your leg");
 
         defeat();
@@ -113,14 +125,36 @@ public class GameJohnsonCaliph extends GameActivity {
 
     private void cleanTheDesk()
     {
-        tvStoryText.setText("\nYou picked need gloves to clean the desk");
-        tvStoryText.setText("1. Get gloves\n2. Don't get gloves");
+        ivStory.setImageResource(R.drawable.im_johnsoncaliph_desk);
+        tvStoryText.setText("\nYou cleaned the desk then... \n1. Cook salad\n2. cook Turkey");
+
+
+        setAllBtnVisible();
+        btn1.setText("Cooking salad");
+        btn2.setText("Cook turkey");
+        btn3.setVisibility(View.INVISIBLE);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cookDinner();
+            }
+        });
+
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cleanTheBed();
+            }
+        });
 
 
     }
 
     private void useYourHands()
     {
+        ivStory.setImageResource(R.drawable.im_johnsoncaliph_desk);
         tvStoryText.setText("You picked to use your hands to clean the desk");
         defeat();
     }
@@ -131,118 +165,285 @@ public class GameJohnsonCaliph extends GameActivity {
 
     private void nogloves()
     {
+        ivStory.setImageResource(R.drawable.im_johnsoncaliph_desk);
         tvStoryText.setText("\nDamm you picked not to use gloves");
         defeat();
     }
     // COOK DINNER
     private void cookDinner()
     {
-        tvStoryText.setText("\nCooking dinner should be easy, so you will be making a salad");
-        tvStoryText.setText("1. Make a veggie salad\n2. Make a meat salad");
+        ivStory.setImageResource(R.drawable.im_johnsoncaliph_making_salad);
+        tvStoryText.setText("\nCooking dinner should be easy, so you will be making which salad");
+
+
+        setAllBtnVisible();
+        btn1.setText("Make a veggie salad");
+        btn2.setText("Make a meat salad");
+        btn3.setVisibility(View.INVISIBLE);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                makeVeggieSalad();
+            }
+        });
+
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                makeMeatSalad();
+            }
+        });
 
 
     }
 
     private void makeVeggieSalad()
     {
-        tvStoryText.setText("\nNice job you're making a veggie salad so you don't have to worry about gettingsick off meat");
+        ivStory.setImageResource(R.drawable.im_johnsoncaliph_veggie_salad);
+        tvStoryText.setText("\nNice job you're making a veggie salad so you don't have to worry about getting sick off meat");
     }
     private void makeMeatSalad()
     {
+        ivStory.setImageResource(R.drawable.im_johnsoncaliph_meat_salad);
         tvStoryText.setText("\nYou picked to make a meat salad but you got sick off of it");
-        tvStoryText.setText("1. leave the salad\n2. clean the salad up");
+
+
+        setAllBtnVisible();
+        btn1.setText("Leave the salad");
+        btn2.setText("Clean the salad up");
+        btn3.setVisibility(View.INVISIBLE);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                leavetheSalad();
+            }
+        });
+
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cleanTheSalad();
+            }
+        });
 
 
     }
     private void leavetheSalad()
     {
+        ivStory.setImageResource(R.drawable.im_johnsoncaliph_leftover_salad);
         tvStoryText.setText("\nYou picked to leave the salad now you are not to feeling too good now");
         defeat();
     }
 
     private void cleanTheSalad()
     {
+        ivStory.setImageResource(R.drawable.im_johnsoncaliph_cleaning_dish);
         tvStoryText.setText("\nYou picked to clean the salad up and you feel better now");
+        defeat();
     }
 
     private void cookTurkey()
     {
-       ivStory.setImageDrawable(R.drawable.);
+        ivStory.setImageResource(R.drawable.im_johnsoncaliph_cook_turkey);
         tvStoryText.setText("\nYou picked to cook turkey but your not to sure on how cook it right");
-        tvStoryText.setText("1. Cook it in the mircowave\n2. Cook it in the oven");
 
+
+        setAllBtnVisible();
+        btn1.setText("Cook it in the mircowave");
+        btn2.setText("Cook it in the oven");
+        btn3.setVisibility(View.INVISIBLE);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cookInTheMicrowave();
+            }
+        });
+
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cookInTheOven();
+            }
+        });
 
     }
 
     private void cookInTheMicrowave()
     {
+        ivStory.setImageResource(R.drawable.im_johnsoncaliph_cook_turkey);
         tvStoryText.setText("\nYou picked to cook it in the microwave but it is still not cooked enough");
         defeat();
     }
     private void cookInTheOven()
     {
+        ivStory.setImageResource(R.drawable.im_johnsoncaliph_microwave);
         tvStoryText.setText("\nYou picked to cook it in the oven and it is cooked perfectly");
-        tvStoryText.setText("1. Season the turkey\n2. Don't season the turkey");
+
+
+        setAllBtnVisible();
+        btn1.setText("Season the turkey");
+        btn2.setText("Don't season the turkey");
+        btn3.setVisibility(View.INVISIBLE);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                seasonTheTurkey();
+            }
+        });
+
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dontSeasonTheTurkey();
+            }
+        });
 
     }
 
     private void seasonTheTurkey()
     {
+        ivStory.setImageResource(R.drawable.im_johnsoncaliph_seasoned_turkey);
         tvStoryText.setText("Its good to see that the turkey is cooked great could've been a bit better though");
         defeat();
     }
     private void dontSeasonTheTurkey()
     {
+        ivStory.setImageResource(R.drawable.im_johnsoncaliph_unseasoned_turkey);
         tvStoryText.setText("\nYou picked not to season the turkey and you are not feeling the best now");
         defeat();
     }
     //CLEAN THE LIVING ROOM
     private void cleanTheLivingRoom()
     {
+        ivStory.setImageResource(R.drawable.im_johnsoncaliph_livingroom);
         tvStoryText.setText("\nCleaning the living room is very important becuase it is one of the biggest rooms in the house");
-        tvStoryText.setText("1. Clean the couch\n2. Clean the TV");
+
+
+        setAllBtnVisible();
+        btn1.setText("Clean the couch");
+        btn2.setText("Clean the TV");
+        btn3.setVisibility(View.INVISIBLE);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cleanTheCouch();
+            }
+        });
+
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cleanTheTV();
+            }
+        });
 
     }
     private void cleanTheCouch()
     {
+        ivStory.setImageResource(R.drawable.im_johnsoncaliph_couch );
         tvStoryText.setText("\nYou picked to clean the couch but you layed down and feel asleep");
         defeat();
     }
 
     private void cleanTheTV()
     {
+        ivStory.setImageResource(R.drawable.im_johnsoncaliph_tv);
         tvStoryText.setText("\nYou picked to clean the TV and it is clean now you are ready to clean out the drawers");
-        tvStoryText.setText("1. Clean the drawers fully\n2.Clean the drawers partially");
+
+
+        setAllBtnVisible();
+        btn1.setText("Clean the drawers fully");
+        btn2.setText("Clean the drawers partially");
+        btn3.setVisibility(View.INVISIBLE);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cleanTheDrawersFully();
+            }
+        });
+
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cleanTheDrawersPartially();
+            }
+        });
 
     }
 
     private void cleanTheDrawersFully()
     {
+        ivStory.setImageResource(R.drawable.im_johnsoncaliph_drawer);
         tvStoryText.setText("\nYou picked to clean the drawers complety and now you are ready to clean the closet");
+        btn1.setText("LET GOOO!!!");
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cleanTheCloset();
+            }
+        });
     }
 
 
     private void cleanTheDrawersPartially()
     {
+        ivStory.setImageResource(R.drawable.im_johnsoncaliph_spider_bite);
         tvStoryText.setText("\nThis didn't work out for because there was a spider in drawer and it bit you");
         defeat();
     }
 
     private void cleanTheCloset()
     {
+        ivStory.setImageResource(R.drawable.im_johnsoncaliph_clean_closest);
         tvStoryText.setText("\nCleaning closet is going to be a big task to handle which would you like to start with first");
         tvStoryText.setText("1. Clean the clothes\n2. Clean the shoes");
+
+        setAllBtnVisible();
+        btn1.setText("Clean the clothes");
+        btn2.setText("Clean the shoes");
+        btn3.setVisibility(View.INVISIBLE);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cleanTheClothes();
+            }
+        });
+
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cleanTheShoes();
+            }
+        });
 
     }
     private void cleanTheClothes()
     {
+        ivStory.setImageResource(R.drawable.im_johnsoncaliph_clothes_clean);
         tvStoryText.setText("\nYou picked to clean the clothes but you missed a few pieces");
         defeat();
     }
 
     private void cleanTheShoes()
     {
+        ivStory.setImageResource(R.drawable.im_johnsoncaliph_shoe);
         tvStoryText.setText("\nYou are doing pretty smooth so far by picking up the shoes which means you are done with evrything great job");
+        defeat();
     }
 
 
@@ -263,15 +464,6 @@ public class GameJohnsonCaliph extends GameActivity {
 
 
 
-    private void getGloves() {
-
-    }
-    private void dontGetGloves() {
-
-    }
-    private void leaveTheSalad() {
-
-    }
 
 
 
